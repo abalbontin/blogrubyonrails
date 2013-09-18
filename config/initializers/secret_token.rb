517f79fb8d54019d8dc561f7bf59a0c030aa617f
abalbontin <alvaro@mmip.es>
2013-09-18 17:58:08 +0200

    Subimos lo hecho hasta ahora del tutorial Getting Strarted, punto 5.12 Updating Posts.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '97ab8a879bdad8b59b41269f7a1577728155d15bea0ab26d3aaec9bdd25fef6efef6f7825529bd2ce3b199205ecd21ec44bc85693fe74491124246354c0ecda4'
